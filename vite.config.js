14f48c114f093ede8171e1d29098e8d983e6c6c1
xingzhi0317 <xwu385@connect.hkust-gz.edu.cn>
2026-04-15 11:39:14 +0800

    Modify Vite config to set base URL
    Updated Vite configuration to include base URL.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  // 重点是加下面这行，把 '你的仓库名' 换成你真实的 GitHub 仓库名字！
+  // 比如你的仓库叫 campusfit-demo，这里就写 '/campusfit-demo/'
+  base: unikorn, 
 })

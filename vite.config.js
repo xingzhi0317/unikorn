a9af2abd27769229d359d7a51777c455553d35e9
xingzhi0317 <xwu385@connect.hkust-gz.edu.cn>
2026-04-15 13:11:23 +0800

    Fix base property syntax in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // 重点是加下面这行，把 '你的仓库名' 换成你真实的 GitHub 仓库名字！
   // 比如你的仓库叫 campusfit-demo，这里就写 '/campusfit-demo/'
-  base: unikorn, 
+  base: 'unikorn', 
 })
